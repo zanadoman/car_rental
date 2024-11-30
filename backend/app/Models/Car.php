d1c390bab8be3aa70a1b9932af61c6ddbc324afe
zanadoman <domanseven@gmail.com>
2024-11-30 14:26:00 +0100

    added additional model relations

--- backend/app/Models/Car.php
+++ backend/app/Models/Car.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Car extends Model
 {
+    public function rents(): HasMany
+    {
+        return $this->hasMany(Rent::class);
+    }
+
+    public function receipts(): HasMany
+    {
+        return $this->hasMany(Receipt::class);
+    }
+
     protected $fillable = [
         'license',
         'brand',
--- backend/app/Models/Receipt.php
+++ backend/app/Models/Receipt.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Receipt extends Model
 {
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
+
     protected $fillable = [
         'user_id',
         'car_id',
